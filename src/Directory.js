e111919601ef869f50bc3736535044414504ed31
Saqif Badruddin <saqif280@gmail.com>
2022-03-08 21:43:59 -0800

    Handle exchange data to render exchange table

--- src/Directory.js
+++ src/Directory.js
@@ -1,22 +1,36 @@
 import { useEffect, useState } from 'react';
 import CoinGeckoErrorAlert from './CoinGeckoErrorAlert';
+import ExchangesTable from './ExchangesTable';
 
 const Directory = () => {
   const [coingeckoRequestFailed, setCoingeckoRequestFailed] = useState(false);
-  const url = 'https://api.coingecko.com/api/v3/exchanges?per_page=10&page=1';
+  // eslint-disable-next-line no-unused-vars
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lastFetchedPage, setLastFetchedPage] = useState();
+  const [exchanges, setExchanges] = useState();
+  const GET_EXCHANGES_URL = `https://api.coingecko.com/api/v3/exchanges?per_page=10&page=${currentPage}`;
 
   useEffect(() => {
-    fetch(url).then((response) => {
-      if (response.status !== 200) {
-        setCoingeckoRequestFailed(true);
-      }
-    });
+    if (lastFetchedPage !== currentPage) {
+      fetch(GET_EXCHANGES_URL)
+        .then((response) => {
+          if (response.status !== 200) {
+            setCoingeckoRequestFailed(true);
+          }
+          setLastFetchedPage(currentPage);
+          return response.json();
+        })
+        .then((data) => {
+          setExchanges(data);
+        });
+    }
   });
 
   return (
     <>
       <h1>Cryptocurrency Exchanges</h1>
       {coingeckoRequestFailed && <CoinGeckoErrorAlert />}
+      {exchanges && <ExchangesTable exchanges={exchanges} />}
     </>
   );
 };
---
+++ src/ExchangesTable.js
@@ -0,0 +1,50 @@
+import Table from 'react-bootstrap/Table';
+import PropTypes from 'prop-types';
+
+const ExchangesTable = ({ exchanges }) => {
+  /**
+   * TODO: Figure out why some urls are not rendering, like Kraken's
+   */
+
+  const renderTableRows = () => {
+    return exchanges.map((exchange) => {
+      return (
+        <tr key={exchange.id}>
+          <td>{exchange.trust_score_rank}</td>
+          <td>
+            <img src={exchange.image} />
+            {exchange.name}
+          </td>
+          <td>{exchange.country}</td>
+          <td>
+            <a href={exchange.url} target="_blank" rel="noreferrer">
+              {exchange.url}
+            </a>
+          </td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <>
+      <Table striped bordered responsive>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Exchange</th>
+            <th>Country</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>{renderTableRows()}</tbody>
+      </Table>
+    </>
+  );
+};
+
+ExchangesTable.propTypes = {
+  exchanges: PropTypes.array.isRequired
+};
+
+export default ExchangesTable;
